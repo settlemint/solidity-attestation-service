7a307d4429d14441bb0ec31298bbae830e95402e
Robbe Verhelst <robbe.verhelst@gmail.com>
2024-06-10 23:33:02 +0200

    fix: test

--- ignition/modules/CustomEAS.ts
+++ ignition/modules/CustomEAS.ts
@@ -1,7 +1,8 @@
 import { buildModule } from "@nomicfoundation/hardhat-ignition/modules";
 
 const EASDeploymentModule = buildModule("EASDeployment", (m) => {
-  const EAS = m.contract("EAS", ["EAS"], {});
+  const ethAddress = "0x1234567890abcdef1234567890abcdef12345678";
+  const EAS = m.contract("EAS", [ethAddress], {});
 
   return { EAS };
 });
