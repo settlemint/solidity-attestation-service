23b4d75ee46f43ca13b7d053b0c02a8b8da01732
Roderik van der Veer <roderik@settlemint.com>
2024-08-15 11:17:05 +0200

    fix: solc versions

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -5,7 +5,7 @@ import type { HardhatUserConfig } from "hardhat/config";
 
 const config: HardhatUserConfig = {
   solidity: {
-    version: "0.8.24",
+    version: "0.8.26",
     settings: {
       viaIR: true,
       optimizer: {
